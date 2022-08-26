aa736fb3f027f1deaf428d045fe18dab64eb466a
Eric Tuvesson <eric.tuvesson@gmail.com>
2022-08-26 10:08:08 +0200

    Update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+echo
+echo "> Installing Noodl CLI..."
 npm install -g @noodl/noodl-cli@2.7.1-beta.220726923055
+echo "> Done"
 
+echo
+echo "> Cloning Noodl Project..."
 noodl-cli project clone "${NOODL_PROJECT_ID}" "./noodl-app"
+echo "> Done"
+
+echo
+echo "> Building Noodl Project..."
 noodl-cli build "./noodl-app" --outFile "./noodl-app-build.zip"
+echo "> Done"
+
